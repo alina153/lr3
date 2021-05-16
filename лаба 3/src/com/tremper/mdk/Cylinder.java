b9320efd9bc1404c577548e6412446ac9edb2cdc
alina153 <72145914+alina153@users.noreply.github.com>
2021-05-16 14:33:25 +0300

    Add files via upload

---
+++ лаба 3/src/com/tremper/mdk/Cylinder.java
@@ -0,0 +1,35 @@
+package com.tremper.mdk;
+
+public class Cylinder extends Circle {
+
+    private double height;
+
+    public Cylinder() {
+    }
+
+    public Cylinder(double height) {
+        this.height = height;
+    }
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * getRadius() * getHeight() * 2;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
